75c002e71311736c785256d2417ca689bacfe0fa
xponen <msafwan86_nnss@hotmail.com>
2018-08-15 16:58:35 +0800

    Add files via upload

---
+++ MSTouchKeyboardTransparent/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace MSTouchKeyboardTransparent
+{
+    static class Program
+    {
+        //reference: 
+        //https://www.dotnetperls.com/main
+        //https://stackoverflow.com/questions/28719056/operator-cannot-be-applied-to-operands-of-type-intptr-and-int-net-3-5
+        //https://www.codeproject.com/Questions/260769/Convert-System-IntPtr-to-System-Runtime-InteropSer
+        //https://www.pinvoke.net/default.aspx/user32.setwindowlong
+        //https://stackoverflow.com/questions/1360758/modifying-opacity-of-any-window-from-c-sharp
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            uint alphaValue = 128;
+            if (args == null || args.Length==0)
+            {
+                
+            }
+            else{
+                String argument = args[0];
+                if (!UInt32.TryParse(argument, out alphaValue))
+                    alphaValue = 128;
+                alphaValue = Math.Min(Math.Max(1, alphaValue), 255);
+            }
+
+            IntPtr KeyboardWnd = GetHWND();
+
+            // Remove WS_EX_LAYERED from this window styles
+            NativeMethods.SetWindowLongPtr(new HandleRef(null, KeyboardWnd), NativeMethods.GWL_EXSTYLE, (IntPtr)((int)(NativeMethods.GetWindowLongPtr(KeyboardWnd, NativeMethods.GWL_EXSTYLE)) & ~NativeMethods.WS_EX_LAYERED));
+            // Ask the window and its children to repaint
+            NativeMethods.RedrawWindow(KeyboardWnd, IntPtr.Zero, IntPtr.Zero, NativeMethods.RedrawWindowFlags.Erase | NativeMethods.RedrawWindowFlags.Invalidate | NativeMethods.RedrawWindowFlags.Frame | NativeMethods.RedrawWindowFlags.AllChildren);
+
+            NativeMethods.SetWindowLongPtr(new HandleRef(null, KeyboardWnd), NativeMethods.GWL_EXSTYLE, (IntPtr)((int)(NativeMethods.GetWindowLongPtr(KeyboardWnd, NativeMethods.GWL_EXSTYLE)) | NativeMethods.WS_EX_LAYERED));
+            NativeMethods.SetLayeredWindowAttributes(KeyboardWnd, 0, (byte)alphaValue, NativeMethods.LWA_ALPHA);
+        }
+
+        private static IntPtr GetHWND()
+        {
+            IntPtr hParent = IntPtr.Zero;
+            int count1 = 0;
+            while (true & count1<10)
+            {
+                hParent = NativeMethods.FindWindowEx(IntPtr.Zero, hParent, "ApplicationFrameWindow", "");
+                if (hParent == IntPtr.Zero) break;
+                IntPtr currChild = NativeMethods.FindWindowEx(hParent, IntPtr.Zero, "Windows.UI.Core.CoreWindow", "Microsoft Text Input Application");
+                if (currChild != IntPtr.Zero) return currChild;
+                count1++;
+            }
+
+            return IntPtr.Zero;
+        }
+    }
+}
---
+++ MSTouchKeyboardTransparent/WindowAPI.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MSTouchKeyboardTransparent
+{
+    internal static class NativeMethods
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(String sClassName, String sAppName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, String lpszClass, String lpszWindow);
+
+        /// <summary>
+        ///     Copies the text of the specified window's title bar (if it has one) into a buffer. If the specified window is a
+        ///     control, the text of the control is copied. However, GetWindowText cannot retrieve the text of a control in another
+        ///     application.
+        ///     <para>
+        ///     Go to https://msdn.microsoft.com/en-us/library/windows/desktop/ms633520%28v=vs.85%29.aspx  for more
+        ///     information
+        ///     </para>
+        /// </summary>
+        /// <param name="hWnd">
+        ///     C++ ( hWnd [in]. Type: HWND )<br />A <see cref="IntPtr" /> handle to the window or control containing the text.
+        /// </param>
+        /// <param name="lpString">
+        ///     C++ ( lpString [out]. Type: LPTSTR )<br />The <see cref="StringBuilder" /> buffer that will receive the text. If
+        ///     the string is as long or longer than the buffer, the string is truncated and terminated with a null character.
+        /// </param>
+        /// <param name="nMaxCount">
+        ///     C++ ( nMaxCount [in]. Type: int )<br /> Should be equivalent to
+        ///     <see cref="StringBuilder.Length" /> after call returns. The <see cref="int" /> maximum number of characters to copy
+        ///     to the buffer, including the null character. If the text exceeds this limit, it is truncated.
+        /// </param>
+        /// <returns>
+        ///     If the function succeeds, the return value is the length, in characters, of the copied string, not including
+        ///     the terminating null character. If the window has no title bar or text, if the title bar is empty, or if the window
+        ///     or control handle is invalid, the return value is zero. To get extended error information, call GetLastError.<br />
+        ///     This function cannot retrieve the text of an edit control in another application.
+        /// </returns>
+        /// <remarks>
+        ///     If the target window is owned by the current process, GetWindowText causes a WM_GETTEXT message to be sent to the
+        ///     specified window or control. If the target window is owned by another process and has a caption, GetWindowText
+        ///     retrieves the window caption text. If the window does not have a caption, the return value is a null string. This
+        ///     behavior is by design. It allows applications to call GetWindowText without becoming unresponsive if the process
+        ///     that owns the target window is not responding. However, if the target window is not responding and it belongs to
+        ///     the calling application, GetWindowText will cause the calling application to become unresponsive. To retrieve the
+        ///     text of a control in another process, send a WM_GETTEXT message directly instead of calling GetWindowText.<br />For
+        ///     an example go to
+        ///     <see cref="!:https://msdn.microsoft.com/en-us/library/windows/desktop/ms644928%28v=vs.85%29.aspx#sending">
+        ///     Sending a
+        ///     Message.
+        ///     </see>
+        /// </remarks>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+
+        public static string GetText(IntPtr hWnd)
+        {
+            // Allocate correct string length first
+            int length = GetWindowTextLength(hWnd);
+            StringBuilder sb = new StringBuilder(length + 1);
+            GetWindowText(hWnd, sb, sb.Capacity);
+            return sb.ToString();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+
+        public const int GWL_EXSTYLE = -20;
+        public const int WS_EX_LAYERED = 0x80000;
+        public const int LWA_ALPHA = 0x2;
+        public const int LWA_COLORKEY = 0x1;
+
+        // This helper static method is required because the 32-bit version of user32.dll does not contain this API
+        // (on any versions of Windows), so linking the method will fail at run-time. The bridge dispatches the request
+        // to the correct function (GetWindowLong in 32-bit mode and GetWindowLongPtr in 64-bit mode)
+        public static IntPtr SetWindowLongPtr(HandleRef hWnd, int nIndex, IntPtr dwNewLong)
+        {
+            if (IntPtr.Size == 8)
+                return SetWindowLongPtr64(hWnd, nIndex, dwNewLong);
+            else
+                return new IntPtr(SetWindowLong32(hWnd, nIndex, dwNewLong.ToInt32()));
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+        private static extern int SetWindowLong32(HandleRef hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
+        private static extern IntPtr SetWindowLongPtr64(HandleRef hWnd, int nIndex, IntPtr dwNewLong);
+
+        public enum GWL : int
+        {
+            GWL_WNDPROC = (-4),
+            GWL_HINSTANCE = (-6),
+            GWL_HWNDPARENT = (-8),
+            GWL_STYLE = (-16),
+            GWL_EXSTYLE = (-20),
+            GWL_USERDATA = (-21),
+            GWL_ID = (-12)
+        }
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+        private static extern IntPtr GetWindowLongPtr32(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLongPtr")]
+        private static extern IntPtr GetWindowLongPtr64(IntPtr hWnd, int nIndex);
+
+        // This static method is required because Win32 does not support
+        // GetWindowLongPtr directly
+        public static IntPtr GetWindowLongPtr(IntPtr hWnd, int nIndex)
+        {
+            if (IntPtr.Size == 8)
+                return GetWindowLongPtr64(hWnd, nIndex);
+            else
+                return GetWindowLongPtr32(hWnd, nIndex);
+        }
+
+        [Flags()]
+        public enum RedrawWindowFlags : uint
+        {
+            /// <summary>
+            /// Invalidates the rectangle or region that you specify in lprcUpdate or hrgnUpdate.
+            /// You can set only one of these parameters to a non-NULL value. If both are NULL, RDW_INVALIDATE invalidates the entire window.
+            /// </summary>
+            Invalidate = 0x1,
+
+            /// <summary>Causes the OS to post a WM_PAINT message to the window regardless of whether a portion of the window is invalid.</summary>
+            InternalPaint = 0x2,
+
+            /// <summary>
+            /// Causes the window to receive a WM_ERASEBKGND message when the window is repainted.
+            /// Specify this value in combination with the RDW_INVALIDATE value; otherwise, RDW_ERASE has no effect.
+            /// </summary>
+            Erase = 0x4,
+
+            /// <summary>
+            /// Validates the rectangle or region that you specify in lprcUpdate or hrgnUpdate.
+            /// You can set only one of these parameters to a non-NULL value. If both are NULL, RDW_VALIDATE validates the entire window.
+            /// This value does not affect internal WM_PAINT messages.
+            /// </summary>
+            Validate = 0x8,
+
+            NoInternalPaint = 0x10,
+
+            /// <summary>Suppresses any pending WM_ERASEBKGND messages.</summary>
+            NoErase = 0x20,
+
+            /// <summary>Excludes child windows, if any, from the repainting operation.</summary>
+            NoChildren = 0x40,
+
+            /// <summary>Includes child windows, if any, in the repainting operation.</summary>
+            AllChildren = 0x80,
+
+            /// <summary>Causes the affected windows, which you specify by setting the RDW_ALLCHILDREN and RDW_NOCHILDREN values, to receive WM_ERASEBKGND and WM_PAINT messages before the RedrawWindow returns, if necessary.</summary>
+            UpdateNow = 0x100,
+
+            /// <summary>
+            /// Causes the affected windows, which you specify by setting the RDW_ALLCHILDREN and RDW_NOCHILDREN values, to receive WM_ERASEBKGND messages before RedrawWindow returns, if necessary.
+            /// The affected windows receive WM_PAINT messages at the ordinary time.
+            /// </summary>
+            EraseNow = 0x200,
+
+            Frame = 0x400,
+
+            NoFrame = 0x800
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool RedrawWindow(IntPtr hWnd, IntPtr lprcUpdate, IntPtr hrgnUpdate, RedrawWindowFlags flags); 
+    }
+}
